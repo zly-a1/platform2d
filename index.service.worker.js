5270978d97a373f396a3b85e80dc2ad94090691c
zly-a1 <129631729+zly-a1@users.noreply.github.com>
2025-06-23 00:53:14 +0000

    Deploying to gh-pages from @ zly-a1/platform2d@6600622aff6a915c1e5eecfb47cb322eb6b93d13 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750582695|5596225';
+const CACHE_VERSION = '1750639979|5662083';
 /** @type {string} */
 const CACHE_PREFIX = 'platform-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
